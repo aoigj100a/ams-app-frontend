7e592f0ddafccc5e81b67065b6f9e399b302e826
aoigj100a <aoigj100@gmail.com>
2022-12-31 14:23:30 +0800

    feat: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    open:true, //vite 專案啟動時自動開啟瀏覽器
+    port:3000, //vite 專案啟動時自定義埠
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
