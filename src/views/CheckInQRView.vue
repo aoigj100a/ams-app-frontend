a19a0dea32655699c0c05d519e928ff0d00b20c3
aoigj100a <aoigj100@gmail.com>
2022-12-31 20:56:35 +0800

    feat : add QRcode

--- src/views/CheckInQRView.vue
+++ src/views/CheckInQRView.vue
@@ -1,28 +1,25 @@
 <script setup lang="ts">
 import router from "../router"
 import DefaultLayout from '../components/layouts/DefaultLayout.vue'
-
+import QRcode from "../components/QRcode.vue"
 const goBack = () => {
     router.back()
 }
 </script>
 <template>
     <DefaultLayout>
-            
-            <div class="flex flex-col h-[calc(100%-72px)]">
-                <div class="flex justify-center items-center flex-col pt-[40px]">
-                    <div class="flex justify-center items-center">
-                        <span class="text-2xl">12月15日</span>
-                    </div>
-                    <div class="flex justify-center items-center">
-                        <span class="text-2xl">09:28</span>
-                    </div>
+        <div class="flex flex-col h-[calc(100%-72px)]">
+            <div class="flex justify-center items-center flex-col pt-[40px] pb-[40px]">
+                <div class="flex justify-center items-center">
+                    <span class="text-2xl">12月15日</span>
                 </div>
-                QR 區域
-                <!-- <div class="flex-1 flex bg-slate-600">
-                    
-                </div> -->
+                <div class="flex justify-center items-center">
+                    <span class="text-2xl">09:28</span>
+                </div>
             </div>
-            
-        </DefaultLayout>
-  </template>
+            <div class="flex justify-center items-center">
+                <QRcode />
+            </div>
+        </div>
+    </DefaultLayout>
+</template>
